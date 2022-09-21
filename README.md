eb6ef548cc607ee5444a35764a56c4f64d84223f
Daniel <beteab.solomon@gmail.com>
2022-09-21 15:38:09 -0400

    Create required files and folders if non-existent
    Allows for an easier installation as it will use the absolute path of StickyNoteWidget.py's location rather than require an Environment Variable. StickyNotes now also have IDs (their titles), which isn't too relevant yet as there is no easy way to create multiple StickyNotes.

--- README.md
+++ README.md
@@ -6,20 +6,21 @@ A virtual sticky note to be placed on a Windows desktop using Python's Tkinter m
 </p>
 
 ## What's New?
-- Fixed the code's _style_ to something more readable. Includes making comments more insightful, better whitespace usage, etc. Also changed the names of several files, mostly removing underscores in favor of CamelCase.
+- Creates the necessary files if they do not exist yet and no longer requires the use of the pesky _Environment Variable_. All makes for a more streamlined installation process.
+- Gave StickyNotes an ID: their title. No functional differences yet other than a change in the window's title and a reworked folder system.
 
 ## Old Changes
+- Fixed the code's _style_ to something more readable. Includes making comments more insightful, better whitespace usage, etc. Also changed the names of several files, mostly removing underscores in favor of CamelCase.
 - Converted StickyNoteWidget from a Procedural Program into an Object-Oriented Program (OOP). This will streamline debugging, adding new features, updating the code style, etc. _Does not change functionality._
 
 ## How to Install
-1. Ensure Python is installed along with the Tkinter library, which is typically included by default.
-2. Create an _Environment Variable_ called `StickyNoteWidgetData`. Its value should be the location of the folder where both `StickyNoteWidget.py` and `textFiles` are stored. For example, your value may be: `C:/Users/NAME/Desktop/widgets/StickyNoteWidget/`. 
-**Must be forward slashes, not back slashes. Must end in a forward slash.**
+1. Ensure Python is installed along with the Tkinter library (typically included by default).
+2. Enjoy!
 
 ## Features
 - Automatically saves text, size, and location of window upon typing or clicking the `X` button. No need to manually save.
 - Automatically saves a backup for each day of the week, 7 backups in total.
-- Easily change attributes such as font size in `textFiles/1/immutableConfig.txt`.
+- Easily change attributes such as font size in `StickyNotes/NAME/immutableConfig.txt`.
 
 <p align="center">
   <img src="images/attributeChangeExample.JPG" alt="Example of changed attributes">
@@ -28,9 +29,7 @@ A virtual sticky note to be placed on a Windows desktop using Python's Tkinter m
 ## Known Issues
 - Cannot handle certain characters well, such as emojis
 - Cannot boldface, italics, etc. for specific snippets of text. It's either all or nothing.
-- Cannot change color of the cursor (it stays black).
 - Spawns a useless black box (Python's terminal window) if opening outside the context of an IDE.
 - Text may not wrap around the window properly.
 - The x and y attributes slightly save the location of the window incorrectly, only solved with `xAdjust` and `yAdjust` in `immutableConfig.txt`.
 - Putting the widget into `ORR mode` renders it impossible to move or close through normal methods; requires ending the task manually in task manager.
-- An extremely rare glitch can occur where the window will spawn outside the bounds of the desktop screen, resulting in it being inaccessible. Fixing this requires one to change the location of the file manually in `config.txt` to something manageable such as `451x257+213+202`.
--- StickyNoteWidget.py
+++ StickyNoteWidget.py
@@ -1,39 +1,57 @@
 import tkinter as tk
-from os import environ
+from os import path, makedirs
 from datetime import datetime as date
 
 
 class StickyNoteWidget:
-    def __init__(self, parent):
+    def __init__(self, parentWindow, title):
 
-        self.parent = parent
+        self.parentWindow = parentWindow
         self.settings = {}
 
-        # make title empty (default is "tk")
-        self.parent.title('')
+        self.parentWindow.title(title)
 
-        # extract StickyNoteWidget directory from Environment Variables. Use this template to pinpoint the directory of every file.
-        directoryTemplate = environ['StickyNoteWidgetData'] + 'textFiles/1/{}'
+        # absolute path of StickyNoteWidget.py + "/StickyNotes/" + title
+        # Use this template to pinpoint the directory of every file.
+        parentDir = path.dirname(path.abspath(__file__)) + "/StickyNotes/" + title
 
         # date.today().isoweekday() will return today's day of the week as an integer. Monday is 1, Tuesday is 2...
-        self.backupToday = directoryTemplate.format('backup' + str(date.today().isoweekday()) + '.txt')
+        self.backupToday = parentDir + '/backup' + str(date.today().isoweekday()) + '.txt'
 
-        self.ramFile = directoryTemplate.format('ram.txt')
-        self.configFile = directoryTemplate.format('config.txt')
-        self.immConfigFile = directoryTemplate.format('immutableConfig.txt')
+        self.ramFile = parentDir + '/ram.txt'
+        self.configFile = parentDir + '/config.txt'
+        self.immConfigFile = parentDir + '/immutableConfig.txt'
+
+        # if this specific StickyNote does not yet exist, create it. If the StickyNotes subfolder happens to not exist either, create that as well
+        if not path.exists(parentDir):
+            makedirs(parentDir)
+
+        # if the ram file doesn't exist, create it
+        if not path.exists(self.ramFile):
+            with open(self.ramFile, 'w') as ramF:
+                ramF.write("Type here!")
+
+        # if either config or immConfig doesn't exist, reset both
+        if not (path.exists(self.configFile) and path.exists(self.immConfigFile)):
+            with open(self.immConfigFile, 'w') as immConfigF:
+                immConfigF.write("# Settings\n\nbgColor: #093553\nbarColor: #2980B9\n\nfontColor: White\nfontFamily: Consolas\nfontSize: 12\nfontWeight: normal\n\n\n# set these to 0 if adjusting the window's spawn location isn't necessary\nxAdjust: -8\nyAdjust: -31\n\n# if unsure, leave at false\nORR: false")
+
+            with open(self.configFile, 'w') as configF:
+                configF.write("300x200+100+100")
+        
 
         # includes all settings: dimensions, location, fontSize, etc.
         self.readSettings()
 
         # set window's dimensions and location
-        self.parent.geometry(self.settings['geo'])
+        self.parentWindow.geometry(self.settings['geometry'])
 
         # a frame with no widgets attached. Provides the extra bar under the titlebar.
-        aestheticFrame = tk.Frame(root)  
+        aestheticFrame = tk.Frame(parentWindow)  
         aestheticFrame.pack(pady=11)
 
         # set the color of aestheticFrame
-        self.parent.configure(background=self.settings['barColor'])
+        self.parentWindow.configure(background=self.settings['barColor'])
         
         # create Text widget
         self.textBox = tk.Text(root,
@@ -53,28 +71,28 @@ class StickyNoteWidget:
 
         self.readTextData()
 
-        # `ORR == True`` will result in no Windows title bar. This perhaps has better aesthetic but makes the window impossible to move and difficult to close (requires the use of "End Task" in Task Manager).
-        self.parent.overrideredirect(self.settings['ORR'])
+        # `ORR == True` will result in no Windows title bar. This perhaps has better aesthetic but makes the window impossible to move and difficult to close (requires the use of "End Task" in Task Manager).
+        self.parentWindow.overrideredirect(self.settings['ORR'])
 
-        # Autosave constantly. `q` is a filler variable; never used.
+        # Autosave constantly. `q` is a required filler variable; never used.
         self.textBox.bind('<KeyRelease>', lambda q: self.saveData())
 
         # Upon user clicking `X`, save the file before closing the window
-        self.parent.protocol('WM_DELETE_WINDOW', lambda: [self.saveData(), root.destroy()])
+        self.parentWindow.protocol('WM_DELETE_WINDOW', lambda: [self.saveData(), root.destroy()])
 
 
     def readTextData(self):
-        with open(self.ramFile, 'r') as ram:
-            self.textBox.insert(tk.INSERT, ram.read()[:-1])
+        with open(self.ramFile, 'r') as ramF:
+            self.textBox.insert(tk.INSERT, ramF.read())
             # -1 is to omit the extra line the program adds
 
 
     def readSettings(self):
-        with open(self.configFile, 'r') as config:
-            self.settings["geo"] = config.read().strip()
+        with open(self.configFile, 'r') as configF:
+            self.settings["geometry"] = configF.read().strip()
 
-        with open(self.immConfigFile, 'r') as immc:
-            for row in immc:
+        with open(self.immConfigFile, 'r') as immConfigF:
+            for row in immConfigF:
                 if row[0] not in ['#', '\n']:
                     x = row.split()
                     self.settings[x[0][:-1]] = x[1]
@@ -83,12 +101,12 @@ class StickyNoteWidget:
     def saveData(self):
         for file in [self.backupToday, self.ramFile]:
             with open(file, 'w') as f:
-                f.write(self.textBox.get('1.0', tk.END))
+                f.write(self.textBox.get('1.0', 'end-1c'))
 
         # save dimensions and location in a specific format (`315x330+467+388`) so as to be easily reused as an argument in self.parent.geometry() upon reopening StickyNoteWidget
         # xAdjust and yAdjust circumvent geometry glitches
-        with open(self.configFile, 'w') as config:
-            config.write(
+        with open(self.configFile, 'w') as configF:
+            configF.write(
                 '{}x{}+{}+{}'.format(
                     str(root.winfo_width()), 
                     str(root.winfo_height()),
@@ -103,6 +121,6 @@ class StickyNoteWidget:
 if __name__ == "__main__":
     root = tk.Tk()
 
-    StickyNoteWidget(root)
+    StickyNoteWidget(root, "mainSticky")
 
     root.mainloop()
